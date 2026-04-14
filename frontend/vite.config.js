a22f00de0bae2713e68f95efc3de3c65e17de707
Elie Andriatsitohaina <eliefenohasina@gmail.com>
2026-04-14 23:52:59 +0300

    serveur live

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,5 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
-  server: { proxy: { '/api': 'http://localhost:4000' } },
+  server: { 
+    proxy: { 
+    '/api': 'http://localhost:4000',
+    '/live': 'http://localhost:3016',
+    '/broadcast': 'http://localhost:3016'
+  } 
+},
 })
